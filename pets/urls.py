61103af669abd726eb9444601bd1ba4e0a0d7ba4
Tushar <tushar@gmail.com>
2023-07-25 23:40:20 +0530

    update

--- pets/urls.py
+++ pets/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='example-view'),
     path('dog',views.dog,name='dog-list-page'),
     path('cat',views.cat,name='cat-list-page'),
-    path('rabbit',views.rabbit,name='bird-list-page'),
+    path('rabbit',views.rabbit,name='rabbit-list-page'),
     path('lion',views.lion,name='lion-list-page'),
     path('tiger',views.tiger,name='tiger-list-page'),
 ]
